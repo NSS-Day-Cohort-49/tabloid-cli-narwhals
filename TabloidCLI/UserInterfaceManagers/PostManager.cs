0543efc2380ee0828346dcca1631939d21688917
Logan Curtis <lcurtis0104@gmail.com>
2021-09-14 13:33:55 -0500

    Added PostManager.cs and called method in MainMenu.cs

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -1,10 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class PostManager
+    public class PostManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) List Posts");
+            Console.WriteLine(" 2) Add Post");
+            Console.WriteLine(" 3) Edit Post");
+            Console.WriteLine(" 4) Remove Post");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Edit();
+                    return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Remove()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
