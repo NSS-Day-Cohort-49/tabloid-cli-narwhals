5d63c04b59c2ab1fa1dfabe34bb0b0521baedecf
tbronny <tristan.bron7@gmail.com>
2021-09-15 14:22:41 -0500

    blog details menu: view details of blog functional

---
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class BlogDetailManager : IUserInterfaceManager
+    {
+        private IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private PostRepository _postRepository;
+        private TagRepository _tagRepository;
+        private int _blogId;
+
+        public BlogDetailManager(IUserInterfaceManager parentUI, string connectionString, int blogId)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _postRepository = new PostRepository(connectionString);
+            _tagRepository = new TagRepository(connectionString);
+            _blogId = blogId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Blog blog = _blogRepository.Get(_blogId);
+            Console.WriteLine($"{blog.ToString()} Details");
+            //View
+            Console.WriteLine(" 1) View");
+            //Add Tag
+            Console.WriteLine(" 2) Add Tag");
+            //Remove Tag
+            Console.WriteLine(" 3) Remove Tag");
+            //View Posts
+            Console.WriteLine(" 4) View Posts");
+            //Return
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    View();
+                    return this;
+                case "2":
+                    AddTag();
+                    return this;
+                case "3":
+                    RemoveTag();
+                    return this;
+                case "4":
+                    ViewPosts();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void View()
+        {
+            Blog blog = _blogRepository.Get(_blogId);
+            Console.WriteLine($"Title: {blog.Title}");
+            Console.WriteLine($"Url: {blog.Url}");
+            Console.WriteLine($"Tags: ");
+            foreach (Tag tag in blog.Tags)
+            {
+                Console.WriteLine(" " + tag);
+            }
+            Console.WriteLine();
+        }
+
+        private void AddTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
