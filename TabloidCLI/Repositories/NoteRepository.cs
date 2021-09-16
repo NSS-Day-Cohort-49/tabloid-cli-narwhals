3300c2b0d842ff0d6bdb5d20e1c9b62012e46e79
Joel Beckum <joelbeckum@gmail.com>
2021-09-16 14:22:53 -0500

    Added Note, NoteRepository, and NoteManager classes

---
+++ TabloidCLI/Repositories/NoteRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    class NoteRepository : DatabaseConnector, IRepository<Note>
+    {
+        private int _postId;
+        public NoteRepository(string connectionString, int postId) : base(connectionString) 
+        {
+            _postId = postId;
+        }
+
+        public List<Note> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ TabloidCLI/UserInterfaceManagers/NoteManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class NoteManager : IUserInterfaceManager
+    {
+        private IUserInterfaceManager _parentUI;
+        private NoteRepository _noteRepository;
+        private int _postId;
+
+        public NoteManager(IUserInterfaceManager parentUI, string connectionString, int postId)
+        {
+            _parentUI = parentUI;
+            _noteRepository = new NoteRepository(connectionString, postId);
+            _postId = postId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Note Menu");
+            Console.WriteLine(" 1) List Notes");
+            Console.WriteLine(" 2) Add Note");
+            Console.WriteLine(" 3) Remove Note");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    ListNotes();
+                    return this;
+                case "2":
+                    AddNote();
+                    return this;
+                case "3":
+                    RemoveNote();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void ListNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveNote()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
